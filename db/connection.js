ace32342857d27cf6554389905b46d3602812d32
Zane <zanebgriffin@gmail.com>
2020-10-23 12:14:38 -0500

    connection

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,13 @@
-const mongoose
+require('dotenv').config()
+const mongoose = require('mongoose')
+const {MONGODBURI} = process.env
+const config = { useUnifiedTopology: true, useNewUrlParser: true}
+const db = mongoose.connection
+
+mongoose.connect(MONGODBURI, config)
+
+db.on('open', () => console.log("connected to mongo"))
+  .on('close', () => console.log('disconnected from mongo'))
+  .on('error', (err) => console.log(err))
+
+module.exports = mongoose
