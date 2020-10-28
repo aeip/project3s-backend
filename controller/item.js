c9bd2fccc98409cf9b5eee84753e87fef9fcc5b9
Zane <zanebgriffin@gmail.com>
2020-10-28 10:41:44 -0500

    item seed

--- controller/item.js
+++ controller/item.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Character = require('../model/character')
 const Item = require('../model/item')
+const itemData = require('../db/itemData.json')
 
 //get all items
 router.get('/', async (req, res) => {
@@ -16,6 +17,16 @@ router.get('/:name', async (req, res) => {
 //make item
 router.post('/', async (req, res) => {
     res.json(await Item.create(req.body))
+})
+
+//seed items
+router.post('/seed', async (req, res) => {
+    res.json(await Item.create(itemData))
+})
+
+//delete all
+router.delete('/', async (req, res) => {
+    res.json(await Item.deleteMany())
 })
 
 module.exports = router
