6abd56b07ff220d6e889462c27e96e6e0fb6b5e9
Zane <zanebgriffin@gmail.com>
2020-10-29 17:05:07 -0500

    fixed deploy bug

--- controller/character.js
+++ controller/character.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const Character = require('../model/character')
-const Item = require('../model/item')
-const Event = require('../model/event')
 const { json } = require('express')
 
 //enter character into game
